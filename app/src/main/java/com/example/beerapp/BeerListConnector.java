a58028764697465e80f876f0c1286396054df4fe
Bartee <bhreniak@gmail.com>
2020-01-06 15:25:48 +0100

    feature/1-download-data-from-api Added beerList.subscribeOn

--- app/src/main/java/com/example/beerapp/BeerListConnector.java
+++ app/src/main/java/com/example/beerapp/BeerListConnector.java
@@ -4,6 +4,8 @@ import com.example.beerapp.API.ApiService;
 import com.example.beerapp.Model.BeerList;
 
 import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -20,5 +22,10 @@ public class BeerListConnector implements BeerListRepository {
         ApiService apiService = retrofit.create(ApiService.class);
 
         Observable<BeerList> beerList = apiService.getBeerList();
+                beerList.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe();
+
+
     }
 }
